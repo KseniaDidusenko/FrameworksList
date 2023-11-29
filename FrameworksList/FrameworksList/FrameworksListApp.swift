b657b5c134e611d3a32b8983bdad5135097a23c9
KseniaDidusenko <ksenia.didusenko@gmail.com>
2023-11-29 21:12:34 +0200

    Initial commit

---
+++ FrameworksList/FrameworksList/FrameworksListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksListApp.swift
+//  FrameworksList
+//
+//  Created by Ksenia on 29.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
